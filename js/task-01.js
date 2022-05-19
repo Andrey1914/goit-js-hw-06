92ae53dfa8fe656b3beda45288533ed44651b948
Andrey1914 <anlikajw@gmail.com>
2022-05-19 07:49:30 +0300

    Update task-01.js

---
+++ js/task-01.js
@@ -0,0 +1,9 @@
+const categoriesItem = document.querySelectorAll('item');
+
+console.log(categoriesItem);
+
+const categoriesTitle = document.querySelectorAll('title');
+
+console.log(categoriesTitle);
+
+
