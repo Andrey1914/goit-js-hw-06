861927aca9b9b1553c47315a03301fb0df4b14e3
Andrey1914 <anlikajw@gmail.com>
2022-05-21 23:29:55 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -13,43 +13,51 @@ const ingredients = [
   "Herbs",
   "Condiments",
 ];
-
-const firstItem = document.createElement("li");
-firstItem.textContent = "Potatoes";
-firstItem.classList.add("item");
-console.log(firstItem);
-
-const secondItem = document.createElement("li");
-secondItem.textContent = "Mushrooms";
-secondItem.classList.add("item");
-console.log(secondItem);
-
-const thirdItem = document.createElement("li");
-thirdItem.textContent = "Garlic";
-thirdItem.classList.add("item");
-console.log(thirdItem);
-
-const fourthItem = document.createElement("li");
-fourthItem.textContent = "Tomatos";
-fourthItem.classList.add("item");
-console.log(fourthItem);
-
-const fifthItem = document.createElement("li");
-fifthItem.textContent = "Herbs";
-fifthItem.classList.add("item");
-console.log(fifthItem);
-
-const sixthItem = document.createElement("li");
-sixthItem.textContent = "Condiments";
-sixthItem.classList.add("item");
-console.log(sixthItem);
-
 const ulIngredients = document.querySelector("#ingredients");
-ulIngredients.append(
-  firstItem,
-  secondItem,
-  thirdItem,
-  fourthItem,
-  fifthItem,
-  sixthItem
-);
+const elements = ingredients.map((ingredient) => {
+  const liEl = document.createElement("li");
+  liEl.classList.add("item");
+  liEl.textContent = ingredient;
+  return liEl;
+});
+ulIngredients.append(...elements);
+
+// const firstItem = document.createElement("li");
+// firstItem.textContent = "Potatoes";
+// firstItem.classList.add("item");
+// console.log(firstItem);
+
+// const secondItem = document.createElement("li");
+// secondItem.textContent = "Mushrooms";
+// secondItem.classList.add("item");
+// console.log(secondItem);
+
+// const thirdItem = document.createElement("li");
+// thirdItem.textContent = "Garlic";
+// thirdItem.classList.add("item");
+// console.log(thirdItem);
+
+// const fourthItem = document.createElement("li");
+// fourthItem.textContent = "Tomatos";
+// fourthItem.classList.add("item");
+// console.log(fourthItem);
+
+// const fifthItem = document.createElement("li");
+// fifthItem.textContent = "Herbs";
+// fifthItem.classList.add("item");
+// console.log(fifthItem);
+
+// const sixthItem = document.createElement("li");
+// sixthItem.textContent = "Condiments";
+// sixthItem.classList.add("item");
+// console.log(sixthItem);
+
+// const ulIngredients = document.querySelector("#ingredients");
+// ulIngredients.append(
+//   firstItem,
+//   secondItem,
+//   thirdItem,
+//   fourthItem,
+//   fifthItem,
+//   sixthItem
+// );
